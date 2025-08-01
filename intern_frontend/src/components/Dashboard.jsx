51976157d2bdcc3c464ac737c7ea568e04b42225
amberabhi <abhiambet0987@gmail.com>
2025-08-02 00:36:20 +0530

    second commit

--- intern_frontend/src/components/Dashboard.jsx
+++ intern_frontend/src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {useNavigate} from 'react-router-dom';
+
 const Dashboard = () => {
   const navigate = useNavigate();
   const [intern, setIntern] = useState(null);
